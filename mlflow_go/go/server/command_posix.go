917dafe97c1f285c2082b57dd56ff24364309220
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2024-07-01 17:52:53 +0100

    Use a job object to stop the entire process group

--- mlflow_go/go/server/command_posix.go
+++ mlflow_go/go/server/command_posix.go
@@ -3,8 +3,16 @@
 package server
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"os/exec"
 	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/mlflow/mlflow-go/mlflow_go/go/config"
 )
 
 func setNewProcessGroup(cmd *exec.Cmd) {
@@ -12,4 +20,32 @@ func setNewProcessGroup(cmd *exec.Cmd) {
 		Setpgid: true,
 		Pgid:    0,
 	}
+}
+
+func launchCommand(ctx context.Context, cfg *config.Config) error {
+	//nolint:gosec
+	cmd := exec.CommandContext(ctx, cfg.PythonCommand[0], cfg.PythonCommand[1:]...)
+	cmd.Env = append(os.Environ(), cfg.PythonEnv...)
+	cmd.Stdout = logrus.StandardLogger().Writer()
+	cmd.Stderr = logrus.StandardLogger().Writer()
+	cmd.WaitDelay = 5 * time.Second //nolint:mnd
+	cmd.Cancel = func() error {
+		logrus.Debug("Sending termination signal to command")
+
+		return cmd.Process.Signal(syscall.SIGTERM)
+	}
+	setNewProcessGroup(cmd)
+
+	logrus.Debugf("Launching command: %v", cmd)
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("command could not launch: %w", err)
+	}
+
+	err := cmd.Wait()
+	if err != nil {
+		return fmt.Errorf("command exited with error: %w", err)
+	}
+
+	return nil
 }
--- mlflow_go/go/server/command_windows.go
+++ mlflow_go/go/server/command_windows.go
@@ -3,10 +3,63 @@
 package server
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"os/exec"
 	"syscall"
+	"time"
+	"unsafe"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/windows"
+
+	"github.com/mlflow/mlflow-go/mlflow_go/go/config"
 )
 
 func setNewProcessGroup(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP}
+}
+
+func launchCommand(ctx context.Context, cfg *config.Config) error {
+	job, err := windows.CreateJobObject(nil, nil)
+	if err != nil {
+		return fmt.Errorf("could not create job object: %w", err)
+	}
+
+	var info windows.JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+	info.BasicLimitInformation.LimitFlags = windows.JOB_OBJECT_LIMIT_KILL_ON_JOB_CLOSE
+	if _, err := windows.SetInformationJobObject(job, windows.JobObjectExtendedLimitInformation,
+		uintptr(unsafe.Pointer(&info)), uint32(unsafe.Sizeof(info))); err != nil {
+		return fmt.Errorf("could not set job object information: %w", err)
+	}
+
+	//nolint:gosec
+	cmd := exec.CommandContext(ctx, cfg.PythonCommand[0], cfg.PythonCommand[1:]...)
+	cmd.Env = append(os.Environ(), cfg.PythonEnv...)
+	cmd.Stdout = logrus.StandardLogger().Writer()
+	cmd.Stderr = logrus.StandardLogger().Writer()
+	cmd.WaitDelay = 5 * time.Second //nolint:mnd
+	cmd.Cancel = func() error {
+		logrus.Debug("Sending termination signal to command")
+
+		return windows.CloseHandle(job)
+	}
+	setNewProcessGroup(cmd)
+
+	logrus.Debugf("Launching command: %v", cmd)
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("command could not launch: %w", err)
+	}
+
+	if err := windows.AssignProcessToJobObject(job, windows.Handle(cmd.Process.Pid)); err != nil {
+		return fmt.Errorf("could not assign process to job object: %w", err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return fmt.Errorf("command exited with error: %w", err)
+	}
+
+	return nil
 }
