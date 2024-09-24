9b65e1f793032d10be5a5f3c076bca4858daf151
Software Developer <7852635+dsuhinin@users.noreply.github.com>
2024-09-24 11:26:14 +0000

    regenerate some files.
    Signed-off-by: Software Developer <7852635+dsuhinin@users.noreply.github.com>

--- pkg/server/routes/artifacts.g.go
+++ pkg/server/routes/artifacts.g.go
@@ -4,8 +4,8 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/mlflow/mlflow-go/pkg/contract/service"
 	"github.com/mlflow/mlflow-go/pkg/server/parser"
+	"github.com/mlflow/mlflow-go/pkg/contract/service"
 )
 
 func RegisterArtifactsServiceRoutes(service service.ArtifactsService, parser *parser.HTTPRequestParser, app *fiber.App) {
