a4ea3ec7eea2aa1f4c6ef9360f503bc85c6ff074
nojaf <florian.verdonck@outlook.com>
2024-09-25 08:50:11 +0000

    Ignore test_log_batch_null_metrics
    Signed-off-by: nojaf <florian.verdonck@outlook.com>

--- tests/override_test_sqlalchemy_store.py
+++ tests/override_test_sqlalchemy_store.py
@@ -7,3 +7,7 @@ def test_log_batch_internal_error(store: SqlAlchemyStore):
 
 def test_log_batch_params_max_length_value(store: SqlAlchemyStore, monkeypatch):
     ()
+
+
+def test_log_batch_null_metrics(store: SqlAlchemyStore):
+    ()
